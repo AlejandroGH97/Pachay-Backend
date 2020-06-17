4167dc6332aac0ee9c364ebb4af38adbacf51703
AlejandroGH97 <alejandrogoicochea@gmail.com>
2020-06-17 14:07:19 -0500

    Indexed email in database to guarantee unique emails.

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -3,6 +3,7 @@ package entities;
 
 import org.springframework.data.annotation.Id;
 import com.google.common.hash.Hashing;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.nio.charset.StandardCharsets;
@@ -13,6 +14,7 @@ public class User {
     @Id
     public String id;
 
+    @Indexed(unique = true)
     public String email;
 
     public String firstName;
