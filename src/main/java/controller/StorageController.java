f790b2ffdf665063bb856f931d5a1cefe567b30f
AlejandroGH97 <alejandrogoicochea@gmail.com>
2020-07-02 11:54:02 -0500

    Upload now recieves an array.

--- src/main/java/controller/StorageController.java
+++ src/main/java/controller/StorageController.java
@@ -10,7 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -38,17 +40,22 @@ public class StorageController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<?> upload(@RequestBody Map<String,Object> files){
+    public ResponseEntity<?> upload(@RequestBody Map<String,List<String>> files){
+
         String ejerciciosPath = "-1";
         String solucionarioPath = "-1";
         String soportePath = "-1";
-        if(files.containsKey("ejercicios")) ejerciciosPath = storageService.uploadFile((String)files.get("ejercicios"));
-        if(files.containsKey("solucionario"))  solucionarioPath = storageService.uploadFile((String)files.get("solucionario"));
-        if(files.containsKey("soporte"))  soportePath = storageService.uploadFile((String)files.get("soporte"));
+
+        if(files.containsKey("ejercicios")) ejerciciosPath = storageService.uploadFile(files.get("ejercicios").get(0));
+        if(files.containsKey("solucionario"))  solucionarioPath = storageService.uploadFile(files.get("solucionario").get(0));
+        if(files.containsKey("soporte"))  soportePath = storageService.uploadFile(files.get("soporte").get(0));
+
         HashMap<String,String> response = new HashMap<>();
+
         if(!ejerciciosPath.equals("-1")) response.put("ejercicios",ejerciciosPath);
         if(!solucionarioPath.equals("-1")) response.put("solucionario",solucionarioPath);
         if(!soportePath.equals("-1")) response.put("soporte",soportePath);
+
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
