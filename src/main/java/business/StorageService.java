292c17382f3b4445c1e881a83e1f0f9a0b057b8a
AlejandroGH97 <alejandrogoicochea@gmail.com>
2020-07-02 12:31:59 -0500

    File download now returns an array.

--- src/main/java/business/StorageService.java
+++ src/main/java/business/StorageService.java
@@ -15,7 +15,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -24,32 +26,36 @@ public class StorageService {
     @Value("${upload.path}")
     private String path;
 
-    public String uploadFile(String file){
+    public ArrayList<String> uploadFile(String file){
         InputStream decodedFile = new ByteArrayInputStream(Base64.getDecoder().decode(file));
-
+        ArrayList<String> response = new ArrayList<>();
         try{
             String filename = UUID.randomUUID().toString().concat(".pdf");
             Files.copy(decodedFile, Paths.get(path.concat(filename)), StandardCopyOption.REPLACE_EXISTING);
             System.out.println("File saved.");
-            return filename;
+            response.add(filename);
+            return response;
         }
         catch (IOException e){
             e.printStackTrace();
             System.out.println("Couldn't save file.");
-            return "-1";
+            response.add("-1");
+            return response;
         }
     }
 
     public Resource downloadFile(String filename){
         String fullpath = path.concat(filename);
-        Path path = Paths.get(fullpath);
         Resource file = null;
+
         try {
+            Path path = Paths.get(fullpath);
             file = new UrlResource(path.toUri());
         }
         catch (MalformedURLException e){
             e.printStackTrace();
         }
+
         return file;
     }
 }
--- src/main/java/controller/StorageController.java
+++ src/main/java/controller/StorageController.java
@@ -9,7 +9,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,28 +35,28 @@ public class StorageController {
 
         }
         catch (IOException e){
-            e.printStackTrace();
+            return ResponseEntity.badRequest()
+                    .body("Invalid request.");
         }
-        return ResponseEntity.badRequest()
-                .body("Invalid request.");
+
     }
 
     @PostMapping("/upload")
     public ResponseEntity<?> upload(@RequestBody Map<String,List<String>> files){
 
-        String ejerciciosPath = "-1";
-        String solucionarioPath = "-1";
-        String soportePath = "-1";
+        ArrayList<String> ejerciciosPath = new ArrayList<>();
+        ArrayList<String> solucionarioPath = new ArrayList<>();
+        ArrayList<String> soportePath = new ArrayList<>();
 
         if(files.containsKey("ejercicios")) ejerciciosPath = storageService.uploadFile(files.get("ejercicios").get(0));
         if(files.containsKey("solucionario"))  solucionarioPath = storageService.uploadFile(files.get("solucionario").get(0));
         if(files.containsKey("soporte"))  soportePath = storageService.uploadFile(files.get("soporte").get(0));
 
-        HashMap<String,String> response = new HashMap<>();
+        HashMap<String,List<String>> response = new HashMap<>();
 
-        if(!ejerciciosPath.equals("-1")) response.put("ejercicios",ejerciciosPath);
-        if(!solucionarioPath.equals("-1")) response.put("solucionario",solucionarioPath);
-        if(!soportePath.equals("-1")) response.put("soporte",soportePath);
+        if(!ejerciciosPath.get(0).equals("-1")) response.put("ejercicios",ejerciciosPath);
+        if(!solucionarioPath.get(0).equals("-1")) response.put("solucionario",solucionarioPath);
+        if(!soportePath.get(0).equals("-1")) response.put("soporte",soportePath);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
